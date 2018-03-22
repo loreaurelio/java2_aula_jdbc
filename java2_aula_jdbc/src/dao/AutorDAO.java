7600ab593fd72f747a4fe8579895bfcc6d2ac466
Loren <Loren@DESKTOP-4FSHOK8>
2018-03-21 21:58:44 -0300

    DELETE CRIADO

---
+++ java2_aula_jdbc/src/dao/AutorDAO.java
@@ -0,0 +1,93 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Autor;
+import util.ConnectionJDBC;
+
+public class AutorDAO {
+    
+    Connection connection;
+    
+    public AutorDAO() throws Exception {
+        //Obtém uma conexão
+        connection = ConnectionJDBC.getConnection();
+    }
+    
+    public void save(Autor autor) throws Exception {
+        String SQL = "INSERT INTO AUTOR(NOME) VALUES (?)";
+        try {
+            PreparedStatement p = connection.prepareStatement(SQL);
+            //p.setInt(1, autor.getAutor_id());
+            p.setString(1, autor.getNome());
+            p.execute();
+            p.close();
+        } catch (SQLException ex) {
+            throw new Exception(ex);
+        }
+    }
+    
+    public void update(Autor autor) throws Exception {
+        String SQL = "UPDATE AUTOR SET NOME=? WHERE AUTOR_ID=?";
+        try {
+            PreparedStatement p = connection.prepareStatement(SQL);
+            p.setString(1, autor.getNome());
+            p.setInt(2, autor.getAutor_id());
+            p.execute();
+            p.close();
+        } catch(SQLException ex) {
+            throw new Exception(ex);
+        }
+    }
+    
+    public void delete(Autor autor) throws Exception {
+     String SQL = "DELETE FROM AUTOR WHERE AUTOR_ID=?";
+     PreparedStatement p;
+        try {
+            p = connection.prepareStatement(SQL);
+            p.setInt(1, autor.getAutor_id());
+            p.execute();
+            p.close();
+        } catch (SQLException ex) {
+            throw new Exception(ex);
+        }
+    }
+    
+    public Autor findById(int id) {
+        return new Autor();
+    }
+    
+    public List<Autor> findAll() throws Exception {
+        // Lista para manter os valores do ResultSet
+        List<Autor> list = new ArrayList<>();
+        Autor objeto;
+        String SQL = "SELECT * FROM AUTOR ORDER BY AUTOR_ID";
+        try {
+            // Prepara a SQL
+            PreparedStatement p = connection.prepareStatement(SQL);
+            // Executa a SQL e mantém os valores no ResultSet rs
+            ResultSet rs = p.executeQuery();
+            // Navega pelos registros no rs
+            while(rs.next()) {
+                // Instancia a classe autor e informa os valores do BD
+                objeto = new Autor();
+                objeto.setAutor_id(rs.getInt("autor_id"));
+                objeto.setNome(rs.getString("nome"));
+                //Inclui na lista
+                list.add(objeto);
+            }
+            rs.close();
+            p.close();
+        } catch (SQLException ex) {
+            throw new Exception(ex);
+        }
+        // Retorna a lista
+        return list;
+    }
+}
--- java2_aula_jdbc/src/util/ConnectionJDBC.java
+++ java2_aula_jdbc/src/util/ConnectionJDBC.java
@@ -1,7 +1,41 @@
 package util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionJDBC {
+
+    private static Connection connection;
+
+    public static Connection getConnection() throws Exception {
+
+        if (connection == null) {
+            try {
+                Class.forName("org.firebirdsql.jdbc.FBDriver");
+            } catch (ClassNotFoundException ex) {
+                throw new Exception(ex);
+            }
+            String servidor = "localhost";
+            String database = "C:\\Users\\Loren\\OneDrive\\Documentos\\Técnico\\bd\\AULA06.fdb";
+            String user = "SYSDBA";
+            String password = "masterkey";
+            String url = "jdbc:firebirdsql:" + servidor + "/3050:" + database + "?encoding=win1252";
+            try {
+                connection = DriverManager.getConnection(url, user, password);
+            } catch (SQLException ex) {
+                throw new Exception(ex);
+            }
+        }
+        return connection;
+    }
+    
     public static void main(String[] args) {
         System.out.println("Teste!");
+        try {
+            ConnectionJDBC.getConnection();
+        } catch (Exception ex) {
+            System.out.println(""+ex.getMessage());
+        }
     }
 }
